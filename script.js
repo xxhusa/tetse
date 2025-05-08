2de7daadbc76cd89280083e6e3b59691a43f3e64
andre <andre.rocha74@etec.sp.gov.br>
2025-05-08 19:26:06 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+document.writeln("pagina em construção")
+let nome="andre"
+document.writeln(`<p>Nome:${nome}<p/>`);
+console.log("Nome:"+nome+" seja bem vindo");
+let numero=12.45;
+console.log(numero);
+let numero1=45;
+console.log(numero1);
+let teste=true;
+console.log(teste);
+const TAXA=13.05;
+console.log(`A taxa é ${TAXA}`);
+console.log(`a nova taxa é ${TAXA}`);
+let nomes=["andre","luis","yully","samuel"];
+console.log(nomes[2]);
+let armazenamento=745;
+document.writeln(`A variavel com o valor ${armazenamento} é do tipo ${typeof armazenamento}`);
+armazenamento="Navio";
+document.writeln(`A variavel com o valor ${armazenamento} é do tipo ${typeof armazenamento}`);
+
